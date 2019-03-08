e7ba9aeb25321b262d881e061430131e2617b3d0
Luiz Paulo Pereira <luizpaulopereiraf@gmail.com>
2019-03-07 21:10:03 -0400

    criando mais duas controllers

---
+++ REDIRECIONAMENTO/Controllers/QuemSomosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace REDIRECIONAMENTO.Controllers
+{
+    public class QuemSomosController : Controller
+    {
+        // GET: QuemSomos
+        public ActionResult Index()
+        {
+            return View("QuemSomos");
+        }
+    }
+}
---
+++ REDIRECIONAMENTO/Controllers/TrabalheConoscoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace REDIRECIONAMENTO.Controllers
+{
+    public class TrabalheConoscoController : Controller
+    {
+        // GET: TrabalheConosco
+        public ActionResult Index()
+        {
+            return View("TrabalheConosco");
+        }
+    }
+}
